c134b91e603ca96c1a75b9c481759e645c377f26
Jeremy Cloarec <jeremycloarec@msn.com>
2024-08-11 16:07:23 +0200

    SearchBar

--- components/InputSearchPokemons.jsx
+++ components/InputSearchPokemons.jsx
@@ -1,27 +1,46 @@
 import React, { useState } from "react";
-import { TextInput, StyleSheet } from "react-native";
+import { TextInput, StyleSheet, View, Image } from "react-native";
 
-export function SearchPokemons({text, onChangeText, handleInputPressed}) {
+export function SearchPokemons({ text, onChangeText, handleInputPressed }) {
     return (
-        <TextInput
-            placeholder="Rechercher un pokemon"
-            style={styles.input}
-            onChangeText={onChangeText}
-            value={text}
-            onSubmitEditing={handleInputPressed}
-        />
+        <>
+            <View style={styles.containerSearch}>
+                <Image
+                    style={styles.searchIcon}
+                    source={require('../assets/search.png')}
+                />
+                <TextInput
+                    placeholder="Rechercher un pokemon"
+                    style={styles.input}
+                    onChangeText={onChangeText}
+                    value={text}
+                    onSubmitEditing={handleInputPressed}
+                />
+            </View>
+        </>
     );
 }
 
 const styles = StyleSheet.create({
+    containerSearch:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginVertical: 10,
+        paddingHorizontal: 12,
+        width: '100%',
+        backgroundColor: 'white',
+        borderRadius: 12,
+
+    },
     input: {
         height: 40,
-        borderWidth: 1,
-        borderColor: 'black',
         padding: 10,
-        borderRadius: 18,
         marginHorizontal: 12,
         marginVertical: 5,
         backgroundColor: 'white'
     },
+    searchIcon: {
+        width: 24,
+        height: 24,
+    }
 });
